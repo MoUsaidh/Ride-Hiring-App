17a327b86d3287d10ea33736927d204611798609
Mohammed Usaidh <usaidh.s@seyasoftech.com>
2025-03-14 15:50:01 +0530

    Initial Commit

---
+++ src/main/java/com/mo/evoride/EvorideApplication.java
@@ -0,0 +1,13 @@
+package com.mo.evoride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvorideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvorideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mo/evoride/EvorideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo.evoride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvorideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
